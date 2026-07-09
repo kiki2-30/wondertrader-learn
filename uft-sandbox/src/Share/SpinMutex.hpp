485b552764312bd523b93c986e0ce0c3fb8e00da
haowenqi <3283763959@qq.com>
2026-07-09 18:22:19 +0800

    uft-sandbox: 从 WT 拆出 UFT 引擎独立编译 (一字未改, gcc 8.4)

---
+++ uft-sandbox/src/Share/SpinMutex.hpp
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <atomic>
+
+class SpinMutex
+{
+private:
+	std::atomic<bool> flag = { false };
+
+public:
+	void lock()
+	{
+		for (;;)
+		{
+			if (!flag.exchange(true, std::memory_order_acquire))
+				break;
+
+			while (flag.load(std::memory_order_relaxed))
+				__builtin_ia32_pause();
+		}
+	}
+
+	void unlock()
+	{
+		flag.store(false, std::memory_order_release);
+	}
+};
+
+class SpinLock
+{
+public:
+	SpinLock(SpinMutex& mtx) :_mutex(mtx) { _mutex.lock(); }
+	SpinLock(const SpinLock&) = delete;
+	SpinLock& operator=(const SpinLock&) = delete;
+	~SpinLock() { _mutex.unlock(); }
+
+private:
+	SpinMutex&	_mutex;
+};
